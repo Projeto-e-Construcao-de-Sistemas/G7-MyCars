d342f6f6186ea14ddb42b3a89ce2817dbeaa77ec
Bernardo <bernardo.agrelos@edu.unirio.br>
2022-06-05 12:45:37 -0300

    Nav bar + redirecionamento para paginas de login e cadastro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login"> Login </Link></li>        
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
