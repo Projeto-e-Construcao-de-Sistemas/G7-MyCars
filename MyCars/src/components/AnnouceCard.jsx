3dd53be274d36effd837218d03cef1d0078bd832
cassianojr <cassiano.jr.2009@hotmail.com>
2022-06-09 14:22:36 -0300

    Now announcements are retrieved from firestore database

--- MyCars/src/components/AnnouceCard.jsx
+++ MyCars/src/components/AnnouceCard.jsx
@@ -2,12 +2,12 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export function AnnouceCard({ title, description, estado, price, yearFabrication, yearVehicle, quilometragem }) {
+export function AnnouceCard({ title, description, estado, price, yearFabrication, yearVehicle, quilometragem, imageUrl }) {
     return (
         <div className='col'>
             <div className="card h-100">
-                {/* <img src="" alt="" className='card-img-top'/> */}
-                <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+                <img src={imageUrl} alt="" className='card-img-top'/>
+                {/* <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg> */}
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
