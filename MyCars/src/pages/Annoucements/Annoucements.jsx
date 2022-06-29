c8770a57fcf1918ecea421b88567b85a5f2991b5
Bernardo <bernardo.agrelos@edu.unirio.br>
2022-06-28 22:13:41 -0300

    visualizar anuncios

---
+++ MyCars/src/pages/Annoucements/Annoucements.jsx
@@ -0,0 +1,41 @@
+import React, { } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { db } from '../../services/firebaseConfig';
+import { addDoc, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { Navbar } from '../../components/Navbar';
+
+
+export const Annoucements = () => {
+
+  const { id } = useParams();
+  const [currentAnnouncement, setCurrentAnnouncement] = useState();
+  const [image, setImage] = useState();
+
+
+
+  useEffect(() => {
+    if(!id || currentAnnouncement) return
+    const getAnnoucement =  async () =>{
+      const announcementDoc = doc(db, "announcement", id);
+      const response = (await getDoc(announcementDoc)).data();
+      setCurrentAnnouncement(response)     
+    }
+    getAnnoucement()
+    .catch(console.error)
+  }, []);
+  
+  console.log(currentAnnouncement)
+
+  return (
+    <div className='root'>
+      <Navbar current="home" />
+       <div className='card w-50 position-absolute top-50 start-50 translate-middle'>
+        <img src={currentAnnouncement.images[0]} alt="" className='' />
+        {/* <p>{image}</p> */}
+      </div>
+    </div>
+  )
+}
+
+export default Annoucements;
