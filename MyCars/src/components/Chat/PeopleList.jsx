b25926027983906fb404233842daa08112093ff6
cassianojr <cassiano.jr.2009@hotmail.com>
2022-07-17 11:46:13 -0300

    fix: nullsafe on chat

--- MyCars/src/components/Chat/PeopleList.jsx
+++ MyCars/src/components/Chat/PeopleList.jsx
@@ -32,7 +32,7 @@ export default function PeopleList({ listAnnouncements, onClick, ownerSet = fals
 
             <ul className="list-unstyled chat-list mt-2 mb-0" >
                 {listAnnouncements.map((announcement) => {
-                    return <ChatAnnouncement key={`${announcement.id} ${announcement.idFrom}`}onClick={onClick} announcementId={announcement?.id} id={announcement?.idFrom} name={`${announcement?.marca} ${announcement?.modelo} `} img={announcement?.images[0]} owner={announcement?.ownerName}/>
+                    return <ChatAnnouncement key={`${announcement?.id} ${announcement?.idFrom}`}onClick={onClick} announcementId={announcement?.id} id={announcement?.idFrom} name={`${announcement?.marca} ${announcement?.modelo} `} img={announcement?.images[0]} owner={announcement?.ownerName}/>
                 })}
             </ul>
         </div>
