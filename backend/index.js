204729058082623f2aa3e31c93c904ee51e0aa5a
cassianojr <cassiano.jr.2009@hotmail.com>
2022-07-28 11:42:41 -0300

    added gitignore and ENV port

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const port = process.env.PORT || 3001;
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -15,8 +17,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
 
 io.on('connection', (socket) => {
